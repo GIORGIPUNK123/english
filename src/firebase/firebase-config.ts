26f4b1ce665f61107e5ad7f0b6e14a5a4cc58778
Giorgi Charashvili <giorgirock@hotmail.com>
2026-02-02 04:22:00 +0400

    put firebase config inside .env

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -11,13 +11,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAPskVYaIAXPTFiu7gANtPpu7exBMrdYPw',
-  authDomain: 'english-learning-396508.firebaseapp.com',
-  projectId: 'english-learning-396508',
-  storageBucket: 'english-learning-396508.appspot.com',
-  messagingSenderId: '639290805058',
-  appId: '1:639290805058:web:7e5e5291afc948ada316db',
-  measurementId: 'G-FSHDLEC620',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
